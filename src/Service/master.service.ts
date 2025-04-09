11ba01178a0b30ef6e8a7dfdea1ed290d1e67674
ahsumon7 <as8370@gmail.com>
2025-04-09 12:16:25 +0600

    final Updated

--- src/Service/master.service.ts
+++ src/Service/master.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { ApiResponceModel } from '../app/model/task';
+import { ApiResponceModel, Task } from '../app/model/task';
 
 @Injectable({
   providedIn: 'root',
@@ -14,4 +14,21 @@ export class MasterService {
   getAllTaskList(): Observable<ApiResponceModel> {
     return this.http.get<ApiResponceModel>(this.apiUrl + 'GetAllTaskList');
   }
+
+
+  addNewTask(obj:Task) : Observable<ApiResponceModel>{
+    return this.http.post<ApiResponceModel>(this.apiUrl + 'CreateNewTask',obj);
+  }
+
+
+
+  updateTask(obj: Task): Observable<ApiResponceModel> {
+    return this.http.put<ApiResponceModel>(this.apiUrl + 'UpdateTask', obj);  // ✅ fixed angle brackets, name & comma
+  }
+  
+  deleteTask(id: number): Observable<ApiResponceModel> {
+    return this.http.delete<ApiResponceModel>(this.apiUrl + 'DeleteTask?itemId=' + id); // ✅ corrected angle brackets and quote
+  }
+
+ 
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,19 @@
 import { Component, inject, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common'; // ✅ Needed for *ngFor, *ngIf
 import { MasterService } from '../Service/master.service';
-import { ApiResponceModel, ITask } from './model/task';
+import { ApiResponceModel, ITask, Task } from './model/task';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule], // ✅ This fixes the issue
+  imports: [CommonModule, FormsModule,], // ✅ This fixes the issue
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
+  taskObj : Task = new Task();
   taskList: ITask[] = [];
   masterService = inject(MasterService);
 
@@ -27,4 +30,71 @@ export class AppComponent implements OnInit {
   trackByItemId(index: number, item: ITask): any {
     return item.itemId;
   }
+
+  addTask(){
+    this.masterService.addNewTask(this.taskObj).subscribe((res: ApiResponceModel)=>{
+    if(res.result){
+      alert('task created sucessfully');
+      this.loadAllTask();
+      this.taskObj = new Task();
+    }
+    }
+  ,error=> {
+    alert('API call error')
+  })
+  }
+
+
+
+  onEdit(item: ITask) {
+    this.taskObj = Object.assign(new Task(), item);
+  
+  
+  
+    setTimeout(() => {
+      const dat = new Date(this.taskObj.dueDate);
+      const day = ('0' + dat.getDate()).slice(-2);
+      const month = ('0' + (dat.getMonth() + 1)).slice(-2);
+      const today = dat.getFullYear() + '-' + month + '-' + day;
+  
+      const input = <HTMLInputElement>document.getElementById('textDate');
+      if (input) {
+        input.value = today;
+      }
+    }, 1000);
+    console.log('Editing Task ID:', this.taskObj.itemId);
+  }
+  updateTask() {
+    this.masterService.updateTask(this.taskObj).subscribe(
+      (res: ApiResponceModel) => {
+        if (res.result) {
+          alert('Task Updated Successfully');
+          this.loadAllTask(); // fixed function name
+          this.taskObj = new Task(); // reset form
+        }
+      },
+      error => {
+        alert('API Call Error');
+      }
+    );
+  }
+  onDelete(id: number) {
+    const isConfirm = confirm("Are you sure you want to delete?");
+    if (isConfirm) {
+      this.masterService.deleteTask(id).subscribe(
+        (res: ApiResponceModel) => {
+          if (res.result) {
+            alert('Task Deleted Successfully');
+            this.loadAllTask();
+          }
+        },
+        error => {
+          alert('API Call Error');
+        }
+      );
+    }
+  }
+  
+
+ 
 }
